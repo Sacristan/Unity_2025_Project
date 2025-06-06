9d4fad59dda47d3c6c5cb2929e5b7168cf2dcdb9
Ģirts Ķesteris <girts.kesteris@gmail.com>
2025-06-06 12:15:32 +0300

    add zombie spawning

---
+++ Assets/ZombieSpawner.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    [SerializeField] private Zombie zombiePrefab;
+    [SerializeField] private uint zombieCount = 5;
+    [SerializeField] private float spawnOffset = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnZombies());
+    }
+
+    IEnumerator SpawnZombies()
+    {
+        for (int i = 0; i < zombieCount; i++)
+        {
+            Vector3 offset = new Vector3(
+                Random.Range(-spawnOffset, spawnOffset),
+                0,
+                Random.Range(-spawnOffset, spawnOffset));
+
+            yield return new WaitForSeconds(0.25f);
+            
+            SpawnObjectAt(transform.position + offset, transform.rotation);
+        }
+    }
+
+    void SpawnObjectAt(Vector3 position, Quaternion rotation)
+    {
+        Instantiate(zombiePrefab.gameObject, position, rotation); 
+    }
+}
