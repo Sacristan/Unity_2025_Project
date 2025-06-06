5a3d17a5b2b98ec82622531075151fe665359fc5
Ģirts Ķesteris <girts.kesteris@gmail.com>
2025-06-06 10:37:46 +0300

    disable player doing stuff when dead

--- Assets/_Game/Src/Player.cs
+++ Assets/_Game/Src/Player.cs
@@ -2,15 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using InfimaGames.LowPolyShooterPack;
 
 public class Player : MonoBehaviour
 {
     public event Action<float> OnHealthChanged;
     public event Action OnDeath;
-    
+
     float health = 100f;
     public float Health => health;
-    
+
     bool isDead = false;
 
     public void OnReceiveDamage(float damage, Animal from)
@@ -18,7 +19,7 @@ public class Player : MonoBehaviour
         Debug.Log($"Player received {damage} damage from {from.gameObject.name}");
         health -= damage;
         OnHealthChanged?.Invoke(health);
-        
+
         if (health <= 0)
         {
             Die();
@@ -29,6 +30,16 @@ public class Player : MonoBehaviour
     {
         if (isDead) return;
         isDead = true;
+        
         OnDeath?.Invoke();
+        
+        Disable(GetComponent<Movement>());
+        Disable(GetComponent<Character>());
+        Disable( GetComponentInChildren<CameraLook>());
+        
+        void Disable(MonoBehaviour behaviour)
+        {
+            if (behaviour) behaviour.enabled = false;
+        }
     }
 }
